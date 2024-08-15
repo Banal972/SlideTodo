f104b06afca4a3b3745783b3c822ba51720aafca
Kim Ji Yu <spbabo97@naver.com>
2024-08-16 04:16:29 +0900

    chore : 컬러 추가

---
+++ constant/color.ts
@@ -0,0 +1,26 @@
+const Color = {
+  slate50: "#F8FAFC",
+  slate100: "#F1F5F9",
+  slate200: "#E2E8F0",
+  slate300: "#CBD5E1",
+  slate400: "#94A3B8",
+  slate500: "#64748B",
+  slate600: "#475569",
+  slate700: "#334155",
+  slate800: "#1E293B",
+  slate900: "#0F172A",
+  slate950: "#020617",
+  blue50: "#EFF6FF",
+  blue100: "#DBEAFE",
+  blue200: "#BFDBFE",
+  blue300: "#93C5FD",
+  blue400: "#60A5FA",
+  blue500: "#3B82F6",
+  blue600: "#2563EB",
+  blue700: "#1D4ED8",
+  blue800: "#1E40AF",
+  blue900: "#1E3A8A",
+  blue950: "#172554",
+};
+
+export default Color;
