7e6f62a375ec8b30c12a99b2e1bc375f8e6eebf0
Kim Ji Yu <spbabo97@naver.com>
2024-08-20 15:12:58 +0900

    feat : 회원가입 기능 추가

--- app/(auth)/sign/index.tsx
+++ app/(auth)/sign/index.tsx
@@ -1,13 +1,38 @@
 import Button from "@/components/common/Button";
 import Input from "@/components/common/Input";
 import Label from "@/components/common/Label";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useState } from "react";
 import Color from "@/constant/color";
+import { Controller, useForm } from "react-hook-form";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "firebaseConfig";
 
 export default function HomeScreen() {
+  const router = useRouter();
+
+  const { control, handleSubmit } = useForm();
+
+  const onSubmitHandler = (data: any) => {
+    const { email, password, pwdConfirm } = data;
+
+    if (!email.includes("@") || password !== pwdConfirm) return;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   const [pwdInShow, setPwdInShow] = useState<{ [key: string]: boolean }>({
     pwd: true,
     confirm: true,
@@ -25,18 +50,55 @@ export default function HomeScreen() {
       <View style={styles.inputContainer}>
         <View>
           <Label>아이디</Label>
-          <Input placeholder="이름을 입력해주세요" />
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholder="이름을 입력해주세요"
+              />
+            )}
+          />
         </View>
         <View>
           <Label>이메일</Label>
-          <Input placeholder="이메일을 입력해주세요" />
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                placeholder="이메일을 입력해주세요"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="email"
+          />
         </View>
         <View>
           <Label>비밀번호</Label>
           <View style={styles.textInputContainer}>
-            <Input
-              secureTextEntry={pwdInShow.pwd}
-              placeholder="비밀번호를 입력해주세요"
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  secureTextEntry={pwdInShow.pwd}
+                  placeholder="비밀번호를 입력해주세요"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="password"
             />
             <Pressable
               style={styles.textInputIcon}
@@ -53,9 +115,21 @@ export default function HomeScreen() {
         <View>
           <Label>비밀번호 확인</Label>
           <View style={styles.textInputContainer}>
-            <Input
-              secureTextEntry={pwdInShow.confirm}
-              placeholder="비밀번호를 다시 한 번 입력해주세요."
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  secureTextEntry={pwdInShow.confirm}
+                  placeholder="비밀번호를 다시 한 번 입력해주세요."
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="pwdConfirm"
             />
             <Pressable
               style={styles.textInputIcon}
@@ -71,7 +145,7 @@ export default function HomeScreen() {
         </View>
       </View>
 
-      <Button label="회원가입" />
+      <Button label="회원가입" onPress={handleSubmit(onSubmitHandler)} />
 
       <Text style={styles.sign}>
         이미 회원이신가요?{" "}
