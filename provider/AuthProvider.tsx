80c8fd5a7e7d020ddd96888239595eeb43e423a7
Kim Ji Yu <spbabo97@naver.com>
2024-09-05 04:24:34 +0900

    fix : 자동 로그인 버그 수정

--- provider/AuthProvider.tsx
+++ provider/AuthProvider.tsx
@@ -1,22 +1,33 @@
-import { ReactNode, useEffect } from "react"
+import { ReactNode, useEffect, useState } from "react"
+import { ActivityIndicator, View } from "react-native"
 
+import Color from "constant/color"
 import { Redirect } from "expo-router"
 import { getStore } from "libs/secureStore"
 import useUserStore from "store/useUserStore"
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const login = useUserStore((state) => state.login)
-  const isAuthenticated = useUserStore((state) => state.isAuthenticated)
+  const { login, isAuthenticated } = useUserStore()
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     async function checkToken() {
       const token = await getStore("accessToken")
       if (token) {
-        login(token)
+        await login(token)
       }
+      setIsLoading(false)
     }
     checkToken()
-  }, [])
+  }, [login])
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 justify-center flex-row p-[10px]">
+        <ActivityIndicator color={Color.blue500} />
+      </View>
+    )
+  }
 
   if (!isAuthenticated) return <Redirect href="/sign-in" />
 
